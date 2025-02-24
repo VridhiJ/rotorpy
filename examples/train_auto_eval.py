273ea55a5b570d9f15d63df5fe0804d356fa51c1
VridhiJ <vridhi@seas.upenn.edu>
2025-02-24 05:42:32 -0500

    training for wind and no wind conditions

--- examples/train_auto_eval.py
+++ examples/train_auto_eval.py
@@ -42,18 +42,45 @@ num_cpu = 4   # for parallelization
 # Choose the weights for our reward function. Here we are creating a lambda function over hover_reward.
 reward_function = lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5})
 
-# Make the environment. For this demo we'll train a policy to command collective thrust and body rates.
-# Turning render_mode="None" will make the training run much faster, as visualization is a current bottleneck.
-env = gym.make("Quadrotor-v0",
-                control_mode ='cmd_motor_speeds',
-                reward_fn = reward_function,
-                quad_params = quad_params,
-                max_time = 5,
-                wind_profile = DrydenGust(dt=1 / 100, sig_wind=np.array([75, 75, 30]),altitude=2.0),
-                world = None,
-                sim_rate = 100,
-                render_mode='None')
 
+
+# Define training scenarios.
+training_scenarios = {
+    "No wind": {
+        "wind_profile": None
+    },
+    "DrydenGust": {
+        "wind_profile": DrydenGust(dt=1 / 100, sig_wind=np.array([75, 75, 30]), altitude=2.0)
+    }
+}
+
+# Common environment parameters. For this demo we'll train a policy to command collective thrust and body rates.
+# Turning render_mode="None" will make the training run much faster, as visualization is a current bottleneck.
+env_config = {
+    "id": "Quadrotor-v0",
+    "control_mode": "cmd_motor_speeds",
+    "reward_fn": lambda obs, act: hover_reward(obs, act, weights={'x': 1, 'v': 0.1, 'w': 0, 'u': 1e-5}),
+    "quad_params": quad_params,
+    "max_time": 5,
+    "world": None,
+    "sim_rate": 100,
+    "render_mode": "None"
+}
+
+
+# Select a scenario
+scenario_names = list(training_scenarios.keys())
+for i, name in enumerate(scenario_names):
+    print(f"{i}: {name}")
+    
+selected_id = int(input("Select scenario: "))
+selected_scenario = training_scenarios[scenario_names[selected_id]]
+
+# Merge selected scenario parameters into env_config
+env_config.update(selected_scenario)
+
+# Make the environment. 
+env = gym.make(**env_config)
 
 # Reset the environment
 observation, info = env.reset(options={'initial_state': 'random','pos_bound': 2, 'vel_bound': 0})
@@ -87,6 +114,6 @@ while True:  # Run indefinitely..
 
     # runs evaluation script periodically
     if auto_eval and epoch_count%(eval_freq) == 0: 
-        evaluate_model(model_path)
+        evaluate_model(model_path, num_timesteps = num_timesteps*(epoch_count+1), selected_scenario = selected_scenario)
 
     epoch_count += 1
